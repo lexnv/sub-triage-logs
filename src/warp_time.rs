2a7f426ecdb336afded64b68898c0a90ad847888
Alexandru Vasile <alexandru.vasile@parity.io>
2024-08-19 17:17:02 +0000

    warp-time: More details into log parsing failures
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- src/warp_time.rs
+++ src/warp_time.rs
@@ -19,7 +19,8 @@ impl WarpTime {
         let date = tokens.next().expect("Invalid date");
         let time = tokens.next().expect("Invalid time");
         let current = format!("{} {}", date, time);
-        chrono::NaiveDateTime::parse_from_str(&current, "%Y-%m-%d %H:%M:%S.%f").unwrap()
+        chrono::NaiveDateTime::parse_from_str(&current, "%Y-%m-%d %H:%M:%S.%f")
+            .expect("Cannot parse provided time from log line")
     }
 
     pub fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
@@ -31,17 +32,17 @@ impl WarpTime {
         let mut lines = result.lines().take(MAX_LINES);
 
         // Warping, Downloading finality proofs.
-
         let start_warp = lines.find(|line| line.contains("Warping, Downloading finality proofs"));
-        let start_warp = Self::extract_time(start_warp.unwrap());
+        let start_warp = Self::extract_time(start_warp.expect("Cannot find start warp time"));
 
         // sync: Warp sync is complete, continuing with state sync.
         let end_warp = lines.find(|line| line.contains("Warp sync is complete"));
-        let end_warp = Self::extract_time(end_warp.unwrap());
+        let end_warp = Self::extract_time(end_warp.expect("Cannot find end warp time"));
 
         // sync: State sync is complete.
         let end_state_sync = lines.find(|line| line.contains("State sync is complete"));
-        let end_state_sync = Self::extract_time(end_state_sync.unwrap());
+        let end_state_sync =
+            Self::extract_time(end_state_sync.expect("Cannot find end state sync time"));
 
         let warp_time = end_warp.signed_duration_since(start_warp).to_std().unwrap();
         let state_sync_time = end_state_sync
