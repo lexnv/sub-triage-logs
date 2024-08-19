2b339ea021b554b1abca4c4f6955bf6d3ccf0ccb
Alexandru Vasile <alexandru.vasile@parity.io>
2024-08-19 12:39:17 +0000

    warp-time: Check the warp time of a running node from file only
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

---
+++ src/warp_time.rs
@@ -0,0 +1,70 @@
+//! Measure warp sync time.
+
+use clap::Parser as ClapParser;
+
+const MAX_LINES: usize = 1000;
+
+#[derive(Debug, ClapParser, Clone)]
+pub struct Config {
+    /// Provide a file for parsing.
+    #[clap(long)]
+    file: String,
+}
+
+pub struct WarpTime;
+
+impl WarpTime {
+    fn extract_time(line: &str) -> chrono::NaiveDateTime {
+        let mut tokens = line.split_whitespace();
+        let date = tokens.next().expect("Invalid date");
+        let time = tokens.next().expect("Invalid time");
+        let current = format!("{} {}", date, time);
+        chrono::NaiveDateTime::parse_from_str(&current, "%Y-%m-%d %H:%M:%S.%f").unwrap()
+    }
+
+    pub fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
+        log::info!("Running warp time: {:?}", config.file);
+
+        let bytes = std::fs::read(config.file)?;
+        let result = String::from_utf8_lossy(&bytes);
+
+        let mut lines = result.lines().take(MAX_LINES);
+
+        // Warping, Downloading finality proofs.
+
+        let start_warp = lines.find(|line| line.contains("Warping, Downloading finality proofs"));
+        let start_warp = Self::extract_time(start_warp.unwrap());
+
+        // sync: Warp sync is complete, continuing with state sync.
+        let end_warp = lines.find(|line| line.contains("Warp sync is complete"));
+        let end_warp = Self::extract_time(end_warp.unwrap());
+
+        // sync: State sync is complete.
+        let end_state_sync = lines.find(|line| line.contains("State sync is complete"));
+        let end_state_sync = Self::extract_time(end_state_sync.unwrap());
+
+        let warp_time = end_warp.signed_duration_since(start_warp).to_std().unwrap();
+        let state_sync_time = end_state_sync
+            .signed_duration_since(end_warp)
+            .to_std()
+            .unwrap();
+        let total = end_state_sync
+            .signed_duration_since(start_warp)
+            .to_std()
+            .unwrap();
+
+        println!();
+        println!();
+        println!("Phase | Time");
+        println!(" -|- ");
+
+        println!("Warp  | {:?}", warp_time);
+        println!("State | {:?}", state_sync_time);
+        println!("Total | {:?}", total);
+
+        println!();
+        println!();
+
+        Ok(())
+    }
+}
